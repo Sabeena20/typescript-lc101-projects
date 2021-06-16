a47844b1721b1e2fed6116429fd1eb395e39fb1c
Sabeena <sabeenarayyan13@gmail.com>
2021-06-16 19:31:00 +0400

    added some code

---
+++ studio/Cargo.ts
@@ -0,0 +1,9 @@
+import { Payload } from './Payload';
+export class Cargo implements Payload{
+  massKg:number;
+  material:string;
+  constructor(masskg:number,material:string){
+      this.massKg = masskg;
+      this.material = material;
+  }
+}
